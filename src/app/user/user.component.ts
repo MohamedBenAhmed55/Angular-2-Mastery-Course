939eb1f128bdaa4c2cac049533a593d4e8ea458d
MohamedBenAhmed55 <mohamed.benahmed@etudiant-fsegt.utm.tn>
2025-05-10 17:49:05 +0100

    Create a configurable component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -12,8 +12,7 @@ export class UserComponent {
   @Input({required: true}) avatar!: string
   @Input({required: true}) name!: string
 
-  // @Output() select = new EventEmitter();
-  select = output<string>()
+  @Output() select = new EventEmitter<string>();
 
   onSelectUser(){
     this.select.emit(this.id)
